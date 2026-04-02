66e430661dce96695faa27413170ec0da99267fc
yusufjon-yu <yusufiyusupov@icloud.com>
2026-04-02 12:49:13 +0500

    Delete Practice8 directory

--- Practice8/config.py
+++
@@ -1,7 +0,0 @@
-DB_CONFIG = {
-    "host":     "localhost",
-    "port":     5432,
-    "dbname":   "phonebook_db",
-    "user":     "postgres",
-    "password": "your_password",   # ← change this
-}
